f5053f8689f520c632289f35daf1d6002495a8bb
skblaz <skrljblaz@gmail.com>
2020-04-14 18:09:53 +0200

    added bc example

---
+++ examples/example_visualization.py
@@ -0,0 +1,32 @@
+from scipy import sparse
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+import san
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def test_simple_ranking():
+    sns.set_style("whitegrid")
+    dataobj = load_breast_cancer()
+    X = dataobj['data']
+    Y = dataobj['target']
+    names = dataobj['feature_names']
+    
+    ## let's overfit, just for demo purposes
+    clf = san.SAN(num_epochs = 32, num_head = 2, batch_size = 8, dropout = 0.2, hidden_layer_size = 32)
+    X = sparse.csr_matrix(X)
+    clf.fit(X, Y)
+    preds = clf.predict(X)
+    global_attention_weights = clf.get_mean_attention_weights()
+    local_attention_matrix = clf.get_instance_attention(X.todense())
+    plt.plot(names, global_attention_weights, label = "Global attention", marker = "x")
+    plt.plot(names, np.mean(local_attention_matrix, axis = 0), label = "Local attention - mean", marker = "x")
+    plt.plot(names, np.max(local_attention_matrix, axis = 0), label = "Local attention - max", marker = "x")
+    plt.legend(loc = 1)
+    plt.xticks(rotation = 90)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+
+    test_simple_ranking()
